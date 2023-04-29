58b11c2e58971270d07ebba818477995e28031bf
Karatsupa <andreykaratsupa@gmail.com>
2023-04-29 19:44:00 +0300

    AddTestsLast

--- src/test/java/ru/netology/statistic/ManagerTest.java
+++ src/test/java/ru/netology/statistic/ManagerTest.java
@@ -22,7 +22,6 @@ public class ManagerTest {
     public void testAdd0() {
         FilmsManager manager = new FilmsManager();
 
-
         String[] actual = manager.findAll();
         String[] expected = {};
         Assertions.assertArrayEquals(expected, actual);
@@ -80,6 +79,47 @@ public class ManagerTest {
 
         String[] actual = manager.findLast();
         String[] expected = {"Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testLast0() {
+        FilmsManager manager = new FilmsManager();
+
+        String[] actual = manager.findLast();
+        String[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testLast1() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.add("Movie 1");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Movie 1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testLast11() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.add("Movie 1");
+        manager.add("Movie 2");
+        manager.add("Movie 3");
+        manager.add("Movie 4");
+        manager.add("Movie 5");
+        manager.add("Movie 6");
+        manager.add("Movie 7");
+        manager.add("Movie 8");
+        manager.add("Movie 9");
+        manager.add("Movie 10");
+        manager.add("Movie 11");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Movie 11", "Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
         Assertions.assertArrayEquals(expected, actual);
     }
 }
